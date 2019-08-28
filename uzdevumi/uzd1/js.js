30a70b3e18160e2449b156489065c0fee24448ee
arvids-prancs <49653843+arvids-prancs@users.noreply.github.com>
2019-08-28 11:08:51 +0300

    done

---
+++ uzdevumi/uzd1/js.js
@@ -0,0 +1,9 @@
+
+function palielinat() {
+    document.getElementById("vertiba").value++;
+}
+function samazinat() {
+    if (document.getElementById("vertiba").value > 0) {
+        document.getElementById("vertiba").value--;
+    }
+}
